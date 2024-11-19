f4fa8ba1103b15c5bc51225a0eebda14437069df
ParthibBiswas10 <parthiblm10@gmail.com>
2024-11-20 01:43:18 +0530

    textChanger day 2

--- src/components/TextBox/textbox.jsx
+++ src/components/TextBox/textbox.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 export default function Textbox() {
+  const [showPopup, setShowPopup] = useState(false);
   const [text, setText] = useState("");
+  const textareaRef = useRef(null);
   const upperCase = () => {
     let newText = text.toUpperCase();
     setText(newText);
@@ -12,6 +14,12 @@ export default function Textbox() {
   };
   const clear = () => {
     setText("");
+  };
+  const copy = () => {
+    textareaRef.current.select(); // Select the text inside the textarea
+    navigator.clipboard.writeText(textareaRef.current.value);
+    setShowPopup(true); // Show popup
+    setTimeout(() => setShowPopup(false), 1000);
   };
   const changing = (e) => {
     setText(e.target.value);
@@ -27,6 +35,7 @@ export default function Textbox() {
           placeholder="Enter Text Here"
           onChange={changing}
           rows="15"
+          ref={textareaRef}
         ></textarea>
       </div>
       <button className="btn btn-primary mt-3 btn-25" onClick={upperCase}>
@@ -38,6 +47,15 @@ export default function Textbox() {
       <button className="btn btn-primary mx-3 mt-3 btn-25" onClick={clear}>
         Clear
       </button>
+
+      <button className="btn btn-primary mx-3 mt-3 btn-25" onClick={copy}>
+        Copy
+      </button>
+      {showPopup && (
+        <div className="absolute top-3 left-1/2 transform -translate-x-1/2 bg-slate-600 text-black p-2 rounded shadow-lg ">
+          Copied to Clipboard!
+        </div>
+      )}
     </div>
   );
 }
