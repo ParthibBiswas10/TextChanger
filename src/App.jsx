002b31e670b91584e188118e33e84f04f4a7149f
ParthibBiswas10 <parthiblm10@gmail.com>
2024-11-22 23:55:05 +0530

    textChanger day 3"

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import { useState } from 'react'
-import Navbar from './components/navbar/navbar'
-import TextBox from './components/TextBox/textbox'
-import './App.css'
+import { useState } from "react";
+import Navbar from "./components/navbar/navbar";
+import TextBox from "./components/TextBox/textbox";
+import "./App.css";
 
 function App() {
- 
   return (
     <>
-    <header>
-    <Navbar/>
-    </header>
-     <div className="container">
-      
-     <TextBox/>
-     </div>
+      <header>
+        <Navbar />
+      </header>
+      <div className="container">
+        <TextBox />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
