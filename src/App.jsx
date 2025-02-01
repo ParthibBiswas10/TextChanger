549d766cc3254e30deeeca0e3bb97517b92b172e
ParthibBiswas10 <parthiblm10@gmail.com>
2025-02-01 20:07:32 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import TextBox from "./components/TextBox/textbox";
 import "./App.css";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+    if (mode === "light") setMode("dark");
+    else setMode("light");
+  };
   return (
     <>
       <header>
-        <Navbar />
+        <Navbar mode={mode} toggleMode={toggleMode} />
       </header>
       <div className="container">
         <TextBox />
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-
-export default function Navbar() {
+export default function Navbar(props) {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+      <nav
+        className={`navbar navbar-expand-lg bg-body-${props.mode} bg-${props.mode}  fixed-top`}
+      >
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
+          <a className={`navbar-brand" href="#"`}>Navbar</a>
           <button
             className="navbar-toggler"
             type="button"
@@ -27,58 +25,26 @@ export default function Navbar() {
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">
-                  Disabled
+                <a className="nav-link text-white" href="#">
+                  About
                 </a>
               </li>
             </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
+          </div>
+          <div className="form-check form-switch">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="flexSwitchCheckDefault"
+              onClick={props.toggleMode}
+            />
+            <label
+              className="form-check-label"
+              htmlFor="flexSwitchCheckDefault"
+            >
+              Mode
+            </label>
           </div>
         </div>
       </nav>
